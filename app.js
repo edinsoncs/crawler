a658db66c418ae2a497ed45ce79e6ff24315173b
edinson <jean_clude_dortosms@hotmail.com>
2016-10-16 21:56:29 -0300

    start

--- app.js
+++ app.js
@@ -1,35 +1,46 @@
 var express = require('express');
 var Crawler = require('crawler');
 var url = require('url');
+var movies = require('./data');
 //Utilizaremos crawler
 
 var servidor = express();
 
 
 var isCrawler = new Crawler({
-	//por defecto vamos a tener 10 conneciones
+	//por defecto vamos a tener 10 connectiones
 	maxConnections: 10,
 	callback: function(err, res, $){
 		$('a').each(function(index, a){
 			var isUrl = $(a).attr('href');
+			console.log(isUrl);
 		});
 	}
 
 });
 
-isCrawler.queue([{
-	uri: 'http://www.viajobien.com',
-	jQuery: false,
+//Limit id cine.com
+for(var i = 1; i < 82999; i++) {
+	
+	isCrawler.queue([{
+		uri: 'http://www.cine.com/peliculas.php?pelicula='+i,
+		jQuery: true,
 
-	callback: function(err, res){
-		console.log(res.body);
-	}
-}]);
+		callback: function(err, res, $){
+			movies(res, $);
+		}
+	}]);
+
+	console.log(i);
+
+}
+
 
 function puerto(num){
 	num = 3000;
 	return num;
 }
+
 servidor.listen(puerto(), function(){
 	console.log('servidor corriendo');
 });
